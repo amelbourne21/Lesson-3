69a8173299ff7233f8c17d3426461297145f27e6
amelbourne21 <93118125+amelbourne21@users.noreply.github.com>
2022-01-10 19:39:55 +1100

    Assignment 3

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This application only uses whole numbers");
+Console.WriteLine("What is your initial deposit?: ");
+var initialDepositInput = Console.ReadLine();
+
+int initialDeposit;
+
+if(int.TryParse(initialDepositInput, out initialDeposit))
+{
+ //input is valid   
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
+
+Console.WriteLine("What is your monthly interest rate? (As a percentage): ");
+var monthlyInterestRateInput = Console.ReadLine();
+
+int monthlyInterestRate;
+
+if(int.TryParse(monthlyInterestRateInput, out monthlyInterestRate))
+{
+ //input is valid   
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
+
+Console.WriteLine("How many months would you like to save for?: ");
+var monthsSavingInput = Console.ReadLine();
+
+int monthsSaving;
+
+if(int.TryParse(monthsSavingInput, out monthsSaving))
+{
+ //input is valid   
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
+
+var savings = initialDeposit * Math.Pow(1 + (monthlyInterestRate/100),monthsSaving);
+
+Console.WriteLine($"Your savings are now ${savings}");
